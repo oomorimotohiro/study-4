ade54a682b35bff27d5f5a0e9679ad298cb55841
YonahaKei <64674150+YonahaKei@users.noreply.github.com>
2020-05-02 12:29:25 +0900

    第3回　C#勉強会成果物

---
+++ Csharp_Study_001/Csharp_Study_001/Main.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Csharp_Study_001
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.calc = new System.Windows.Forms.Button();
+            this.left = new System.Windows.Forms.TextBox();
+            this.right = new System.Windows.Forms.TextBox();
+            this.ope = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // calc
+            // 
+            this.calc.Location = new System.Drawing.Point(92, 102);
+            this.calc.Name = "calc";
+            this.calc.Size = new System.Drawing.Size(65, 23);
+            this.calc.TabIndex = 0;
+            this.calc.Text = "計算";
+            this.calc.UseVisualStyleBackColor = true;
+            this.calc.Click += new System.EventHandler(this.calc_Click);
+            // 
+            // left
+            // 
+            this.left.Location = new System.Drawing.Point(22, 44);
+            this.left.Name = "left";
+            this.left.Size = new System.Drawing.Size(65, 19);
+            this.left.TabIndex = 1;
+            // 
+            // right
+            // 
+            this.right.Location = new System.Drawing.Point(164, 44);
+            this.right.Name = "right";
+            this.right.Size = new System.Drawing.Size(65, 19);
+            this.right.TabIndex = 2;
+            // 
+            // ope
+            // 
+            this.ope.FormattingEnabled = true;
+            this.ope.Items.AddRange(new object[] {
+            "＋",
+            "－",
+            "×",
+            "÷"});
+            this.ope.Location = new System.Drawing.Point(108, 44);
+            this.ope.Name = "ope";
+            this.ope.Size = new System.Drawing.Size(35, 20);
+            this.ope.TabIndex = 3;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 137);
+            this.Controls.Add(this.ope);
+            this.Controls.Add(this.right);
+            this.Controls.Add(this.left);
+            this.Controls.Add(this.calc);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "C#研修001";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox left;
+        private System.Windows.Forms.TextBox right;
+        private System.Windows.Forms.ComboBox ope;
+        private System.Windows.Forms.Button calc;
+    }
+}
+
---
+++ Csharp_Study_001/Csharp_Study_001/Main.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Csharp_Study_001
+{
+    public partial class Main : Form
+    {
+        const string tasu = "＋";
+        const string hiku = "－";
+        const string kake = "×";
+        const string waru = "÷";
+            
+        public Main()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// 計算ボタンクリックイベント
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void calc_Click(object sender, EventArgs e)
+        {
+             string hidari = left.Text;
+             string migi = right.Text;
+             string enzan = ope.Text;
+             decimal kotae;
+
+
+            try {
+                if ((hidari == "") || (migi == "") || (enzan == ""))
+                {
+                    ShowErrMsg("未記入の項目が存在します");
+                    return;
+
+                }
+
+                switch (enzan)
+                {
+                    case tasu:
+                        kotae = decimal.Parse(hidari) + decimal.Parse(migi);
+                        break;
+
+                    case hiku:
+                        kotae = decimal.Parse(hidari) - decimal.Parse(migi);
+                        break;
+
+                    case kake:
+                        kotae = decimal.Parse(hidari) * decimal.Parse(migi);
+                        break;
+
+                    case waru:
+                        kotae = decimal.Parse(hidari) / decimal.Parse(migi);
+                        break;
+
+                    default:
+                        throw new System.FormatException();
+                }
+
+                ShowInfoMsg(kotae.ToString());
+
+            }
+            catch (System.DivideByZeroException)
+            {
+                ShowErrMsg("0では割れません");
+            }
+            catch (System.OverflowException)
+            {
+                ShowErrMsg("値が大きすぎます");
+            }
+            catch (System.FormatException)
+            {
+                ShowErrMsg("入力内容が不正です");
+            }
+
+
+
+            
+        }
+
+        /// <summary>
+        /// 通知ダイアログ表示
+        /// </summary>
+        /// <param name="message">メッセージ</param>
+        private static void ShowInfoMsg(string message)
+        {
+            MessageBox.Show(message,
+                            "計算結果",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information,
+                            MessageBoxDefaultButton.Button1);
+        }
+
+        /// <summary>
+        /// エラーダイアログ表示
+        /// </summary>
+        /// <param name="message">メッセージ</param>
+        private static void ShowErrMsg(string message)
+        {
+            MessageBox.Show(message,
+                            "計算失敗",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error,
+                            MessageBoxDefaultButton.Button1);
+        }
+
+    }
+}
