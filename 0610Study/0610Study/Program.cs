36cc7f69f1ecef5895240df494eb5cf76d35c4dd
大森基裕 <m.oomori@dcom-net.jp>
2020-06-15 13:21:05 +0900

    早期リターン

--- 0610Study/0610Study/Program.cs
+++ 0610Study/0610Study/Program.cs
@@ -10,40 +10,36 @@ namespace _0610Study
             {
                 Console.Write("値を入力してください：");
                 int num =Int32.Parse(Console.ReadLine());
-
-                if (num > 0 && num <= 100)
-                {
-                    for (int count = 1; count <= num; count++)
-                    {
-                        if (count % 3 == 0 && count % 5 == 0)
-                        {
-                            Console.WriteLine("Fizz Buzz");
-                        }
-                        else if (count % 3 == 0)
-                        {
-                            Console.WriteLine("Fizz");
-                        }
-                        else if (count % 5 == 0)
-                        {
-                            Console.WriteLine("Buzz");
-                        }
-                        else
-                        {
-                            Console.WriteLine(count);
-                        }
-                    }
-                }
-                else
+                if (num <= 0 || num > 100) 
                 {
                     Console.WriteLine("error");
+                    return;
                 }
-            }catch(Exception)
+
+                for (int count = 1; count <= num; count++)
+                {
+                    if (count % 3 == 0 && count % 5 == 0)
+                    {
+                        Console.WriteLine("Fizz Buzz");
+                    }
+                    else if (count % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                    else if (count % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(count);
+                    }
+                }
+            } 
+            catch(Exception)
             {
                 Console.WriteLine("値が不正です。数字を入力してください");
             }
-            
-            
-            
         }
     }
 }
